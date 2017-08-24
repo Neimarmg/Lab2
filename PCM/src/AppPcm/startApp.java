39efba8414861bbff3f057f2e14af05f16283d0f
Neimarmg <neimarmg@hotmail.com>
2017-08-24 12:07:43 -0300

    Primeiro comit do projeto PCM

---
+++ PCM/src/AppPcm/startApp.java
@@ -0,0 +1,17 @@
+package AppPcm;
+
+import V.View;
+import V.Utilitarios.Dia;
+import V.Utilitarios.Pressagios;
+
+public class startApp {
+
+	public static void main(String[] args) {
+		
+		View.msgcr("\n\nPrograma de consultirio médico\n\n" );
+		Dia.defineSaudacao();
+		View.msgl();
+
+	}
+
+}
