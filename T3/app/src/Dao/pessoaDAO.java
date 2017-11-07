03c460c3b67aca676503e5cb6ebf17f5d1eefb31
Neimar <neimarmg@hotmail.com>
2017-11-07 21:01:03 -0200

    Criaçao do lista de consulta

--- T3/app/src/Dao/pessoaDAO.java
+++ T3/app/src/Dao/pessoaDAO.java
@@ -8,6 +8,9 @@ import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.ResultSet;
 
 
 
@@ -75,4 +78,94 @@ public class pessoaDAO implements Serializable{
             e.printStackTrace();
         }
     }
+    
+    
+    public List<Pessoa> listarTodos(){ //procurar todos nao tem parametro
+			
+        List<Pessoa> listaDePessoas = new ArrayList<Pessoa>();
+
+        //montando o sql
+        ConnectionFactory.setSql("SELECT * FROM Pessoa");
+
+        try{
+            //preparando PreparedStatment com o SQL
+            //quem prepara eh o connection
+            PreparedStatement prepara = con.prepareStatement(sql);
+            //executando ---CONSULTA--- no banco de dados o comando sql
+            ResultSet resultado = prepara.executeQuery(); //retorna resultado da consulta da query -> tipo ResultSet
+
+            while(resultado.next()){ //buscando valor das colunas, registro por registro
+                Pessoa pessoa  = new Pessoa();
+    
+                pessoa.setCodPessoa(codPessoa);         
+                pessoa.setNome("nome");
+                pessoa.setCidade(View.digitaString("cidade"));
+                pessoa.setCodTipoPessoa(View.digitaNumero("codTipoPessoa"));
+                pessoa.setCodProfissao(View.digitaNumero("codProfisao"));
+                pessoa.setCpf(View.digitaString("cpd"));
+                pessoa.setEmail(View.digitaString("E-mail"));
+                pessoa.setAtiva(View.digitaString("Ativa"));     
+                
+                listaDePessoas.add(pessoa);
+            }
+            prepara.close();
+
+            System.out.println("Listando Todos os Registros");
+
+            } catch(SQLException e){ 
+                    //se comando sql nao estiver correto ira imprimir o erro gerado
+                    e.printStackTrace();
+            }
+
+            return listaDePessoas;
+    }
 }
+
+
+
+
+
+
+
+
+/*public List<Usuario> listarTodos(){ //procurar todos nao tem parametro
+			
+			List<Usuario> listaDeUsuarios = new ArrayList<Usuario>();
+			
+			//montando o sql
+			String sql = "SELECT * FROM usuario";
+			
+			try{
+				//preparando PreparedStatment com o SQL
+				//quem prepara eh o connection
+				PreparedStatement prepara = con.prepareStatement(sql);
+				//executando ---CONSULTA--- no banco de dados o comando sql
+				ResultSet resultado = prepara.executeQuery(); //retorna resultado da consulta da query -> tipo ResultSet
+				
+				while(resultado.next()){ //buscando valor das colunas, registro por registro
+					
+					Usuario usu  = new Usuario();
+
+					int id = resultado.getInt("id");
+					String nome = resultado.getString("nome");
+					String login = resultado.getString("login");
+					String senha = resultado.getString("senha");
+
+					usu.setId(id);
+					usu.setNome(nome);
+					usu.setLogin(login);
+					usu.setSenha(senha);
+
+					listaDeUsuarios.add(usu);
+				}
+				prepara.close();
+
+				System.out.println("Listando Todos os Registros");
+
+			} catch(SQLException e){ 
+				//se comando sql nao estiver correto ira imprimir o erro gerado
+				e.printStackTrace();
+			}
+			
+			return listaDeUsuarios;
+		}*/
