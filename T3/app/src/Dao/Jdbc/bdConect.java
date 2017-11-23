fc6209657a462c80edca1da50ffe6bfa763bb9e8
Neimar <neimarmg@hotmail.com>
2017-11-23 18:01:08 -0200

    Javafx

--- T3/app/src/Dao/Jdbc/bdConect.java
+++ T3/app/src/Dao/Jdbc/bdConect.java
@@ -18,7 +18,7 @@ public class bdConect {
     private final static String SENHA = "neimar";*/
     
     
-    private final static String HOST = "mysql.hostinger.com.br";
+    private final static String HOST = "mysql.hostinger.es";
     private final static String PORT = "3306";
     private final static String BD = "id3662634_bd_t3"; 
     private final static String URL = "jdbc:mysql://"+HOST+":"+PORT+"/"+BD;
